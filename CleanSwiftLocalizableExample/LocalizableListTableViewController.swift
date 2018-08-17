b9875a95fb3caad6bfce652e6a5bae41bc51f3ed
Gino Wu <gwu@stashinvest.com>
2018-08-17 18:15:56 -0400

    Initial commit

---
+++ CleanSwiftLocalizableExample/LocalizableListTableViewController.swift
@@ -0,0 +1,37 @@
+//
+//  LocalizableListTableViewController.swift
+//  CleanSwiftLocalizableExample
+//
+//  Created by Gino Wu on 8/17/18.
+//  Copyright © 2018 Gino Wu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LocalizableListTableViewController: UITableViewController {
+
+    let fruits: [String] = [
+        NSLocalizedString("LocalizableListTableViewController:Fruit:Apple", comment: "Apple title"),
+        NSLocalizedString("LocalizableListTableViewController:Fruit:Orange", comment: "Orange title"),
+        NSLocalizedString("LocalizableListTableViewController:Fruit:Grape", comment: "Grape title"),
+        NSLocalizedString("LocalizableListTableViewController:Fruit:Strawberry", comment: "Strawberry title")
+    ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fruits.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else { fatalError() }
+        cell.textLabel?.text = fruits[indexPath.row]
+        return cell
+    }
+
+}
---
+++ CleanSwiftLocalizableExample/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  CleanSwiftLocalizableExample
+//
+//  Created by Gino Wu on 8/17/18.
+//  Copyright © 2018 Gino Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+
+}
+
